837706c0ce408fe2c01a2938696ca67218376029
Alexander Bochkov <abochkov@mail.ru>
2020-06-24 21:30:20 +0300

    Практическое задание к уроку 5

--- task6.py
+++ task6.py
@@ -2,17 +2,14 @@ my_dict = {}
 
 with open("text_6.txt", "r", encoding="utf-8") as my_file:
     my_data = my_file.readlines()
-    print(my_data)
 
 for my_str in my_data:
     my_record = my_str.split()
-    print(my_record)
     num_lessons = 0
     for j in range(1,len(my_record)):
         if my_record[j] != "-":
             num_lessons += int(my_record[j][0:my_record[j].find("(")])
-            print(int(my_record[j][0:my_record[j].find("(")]))
-    my_dict.update(dict(my_record[0]=num_lessons))
+    my_dict[my_record[0]] = num_lessons
 
 
-
+print(my_dict)
---
+++ task7.py
@@ -0,0 +1,24 @@
+import json
+
+my_list = []
+profit_list = {}
+average_profit = {}
+total_profit = 0
+profitable = 0
+
+with open("text_7.txt", "r", encoding="utf-8") as my_file:
+    for line in my_file:
+        my_record = line.split()
+        profit = float(my_record[2]) - float(my_record[3])
+        if profit >= 0:
+            total_profit += profit
+            profitable += 1
+        profit_list[my_record[0]] = profit
+
+average_profit["average_profit"] = total_profit / profitable
+
+my_list.append(profit_list)
+my_list.append(average_profit)
+
+with open("text_7(mine).json", "w", encoding="utf-8") as my_json:
+    json.dump(my_list, my_json)
